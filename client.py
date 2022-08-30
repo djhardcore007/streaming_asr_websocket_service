4227039526ff516de317a665cd16e631e732b28b
Denglin Jiang <denglinjiang@Denglins-MBP.lan>
2022-08-29 17:43:34 -0700

    asr socket server

--- client.py
+++ client.py
@@ -1,14 +1,10 @@
 import socket
 import wave
-import base64
 import time
-# import pickle             # a way of serialized
 
 
-HEADER = 64                 # every msg header is a 64-byte header telling how big the msg size to the server.
 PORT = 5050
-FORMAT = 'utf-8'            # you could use any port which is not being used.  # port 8080 is more like the http ports.
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = b"!DISCONNECT"
 SERVER = "0.0.0.0"
 # "10.111.136.253"   # use your ipv4 address # alternative way # SERVER = socket.gethostbyname("localhost")  # get the ipv4 addess automatically
 ADDR = (SERVER, PORT)       # needs a tuple to bind into server
@@ -18,25 +14,19 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 
-def send(msg):
+def send(msg: bytes):
     """
     come up w ur own way to send the msg.
     you could sen msg in the following format:
     1. utf-8 format string
     2. serializing obj using pickle
     """
-    message = str(msg).encode(FORMAT)                   # follow the server's protocol for msg format.
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))   # to make sure the header is 64-bytes long, so we need to pad.
-
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))             # 2048 is a large number that we will receive whatever msg is received.
+    client.sendall(msg)                                 # avoid all the hassle and sendall bytes
 
 
 WAV_PATH = 'data/87984097648596_202204032122368798409764859620debacde9b225730fb5e8d9e1cc8071b.amr'
-chunk_size = 20
+chunk_size = 800
+
 i = 0
 start_time = time.time()
 
@@ -48,9 +38,8 @@ with wave.open(WAV_PATH) as wav_file:
         chunk = wav_file.readframes(chunk_size)
         if not chunk: # ending...
             break
-        # serialize data
-        data = base64.b64encode(chunk).decode('utf-8')       # client sends this to server
-        send(data)
+        send(chunk)          # send audio directly
+
 
 print(time.time()-start_time)
 send(DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-import argparse
-import base64
-import json
-import time
-from pathlib import Path
-import wave
 import socket
 import threading
 
+import torch
+
+from models import frames_to_tensor, ASRInference
+
+
+DEVICE = device = f"cuda:0" if torch.cuda.is_available() else "cpu"
+HUGGINGFACE_FOLDER = 'models/huggingface-hub-ciempiess16k'
+MODEL_PATH = 'models/best_model.tar'
+ORI_SR = 8000
+TARGET_SAMPLING_RATE = 16000
+
+asr = ASRInference(device=DEVICE, huggingface_folder=HUGGINGFACE_FOLDER, model_path=MODEL_PATH, target_sampling_rate=TARGET_SAMPLING_RATE)
 
 HEADER = 64               # every msg header is a 64-byte header telling how big the msg size to the server.
 PORT = 5050                 # you could use any port which is not being used.  # port 8080 is more like the http ports.
@@ -14,8 +20,7 @@ SERVER = "0.0.0.0"
 # "10.111.136.253"   # use your ipv4 address # alternative way # SERVER = socket.gethostbyname("localhost")  # get the ipv4 addess automatically
 ADDR = (SERVER, PORT)       # needs a tuple to bind into server
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-
+DISCONNECT_MESSAGE = b"!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET tells we pick ipv4 address, SOCK_STREAM mean we send streaming data.
 server.bind(ADDR)
@@ -30,16 +35,26 @@ def handle_client(conn, addr):
     print(f'[NEW CONNECTION] {addr} connected.')
 
     connected = True
+    chunk_size = 800
+    buffer = []
+    i = 0
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT) # first msg tells us the length of the msg.
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
+        while connected and i < 3 * 8000:
+            msg = conn.recv(chunk_size)
+            buffer.append(msg)
+            i += chunk_size
             if msg == DISCONNECT_MESSAGE:
                 connected = False
-
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT)) # send msg back to the client
+                break
+            # conn.send("Msg received".encode(FORMAT)) # send msg back to the client
+        frames = b''.join(buffer)
+        if frames:
+            audio_window = frames_to_tensor(frames, ORI_SR, TARGET_SAMPLING_RATE)
+            partial_transcript = asr.transcribe(audio_window)
+            print(f'[TRANSCRIBING] {partial_transcript}')
+            conn.send(f"partial_transcript".encode('utf-8')) # send msg back to the client
+        i = 0
+        buffer = []
 
     conn.close()
 
@@ -59,37 +74,3 @@ def start():
 
 print('[STARTING] starting server is starting...')
 start()
-
-
-# import torch
-
-# from models import frames_to_tensor, ASRInference
-# DEVICE = device = f"cuda:0" if torch.cuda.is_available() else "cpu"
-# HUGGINGFACE_FOLDER = 'models/huggingface-hub-ciempiess16k'
-# MODEL_PATH = 'models/best_model.tar'
-# TARGET_SAMPLING_RATE = 16000
-# DURATION = 3
-
-# asr = ASRInference(device=DEVICE, huggingface_folder=HUGGINGFACE_FOLDER, model_path=MODEL_PATH, target_sampling_rate=TARGET_SAMPLING_RATE)
-# audio_bytes_buffer = []
-# buffer_size = DURATION * TARGET_SAMPLING_RATE           # num frames to receive
-# i = 0
-# while i < buffer_size:
-#     print(i)
-#     msg_length = conn.recv(HEADER).decode(FORMAT)       # first msg tells us the length of the msg.
-#     if msg_length:
-#         msg_length = int(msg_length)
-#         msg = conn.recv(msg_length).decode(FORMAT)      # receive str
-#         audio_bytes = base64.b64decode(bytes(msg, 'utf-8'))  # turn str to bytes
-#         audio_bytes_buffer.append(audio_bytes)
-#         i += msg_length
-#         if msg == DISCONNECT_MESSAGE:
-#             connected = False
-#             break
-
-# frames = b''.join(audio_bytes_buffer)                   # recvall
-# print(f"[{addr}] audio_chunk")
-# audio_chunk = frames_to_tensor(frames, TARGET_SAMPLING_RATE, TARGET_SAMPLING_RATE)
-# print(audio_chunk.shape)
-# partial_transcript = asr.transcribe(audio_chunk)
-# print(f'[TRANSCRIBING] {partial_transcript}')
